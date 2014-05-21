31a8d84747b4dc7471f5a40e016a8dd6f63fb489
loadletter <loadletter@users.noreply.github.com>
2014-05-21 15:36:35 +0200

    Update bootstrapdl.sh

--- bootstrapdl.sh
+++ bootstrapdl.sh
@@ -1,1 +1,1 @@
-wget -O - http://kirara.ca/poison/Nodefile | awk '{print $1 " " $2 " " $3}' > /tmp/DHTservers
+wget --no-check-certificate -O - http://kirara.ca/poison/Nodefile | awk '{print $1 " " $2 " " $3}' > /tmp/DHTservers
